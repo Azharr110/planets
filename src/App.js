1e01e24cd0ba2cfc125d9d78ab1c94972cbbc6d8
Azhar <abdazhar786@gmail.com>
2022-07-26 16:40:03 +0530

    basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// function App() {
+//   return (
+//     <div className="My_planets">
+//       "Earth", "Mars", "Jupiter", "Venus", "Saturn", "SMAC", "Neptune", "Pluto",
+//       "Sun"
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
+  let planets = [
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Venus",
+    "Saturn",
+    "SMAC",
+    "Neptune",
+    "Pluto",
+    "Sun",
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="My_planets">
+        {planets.map((name) => (
+          <h2>{name}</h2>
+        ))}
+      </div>{" "}
     </div>
   );
 }
-
 export default App;
