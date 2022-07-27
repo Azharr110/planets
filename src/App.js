8544043bc95174d2de50f4abb6fcb748b917607f
Azhar <abdazhar786@gmail.com>
2022-07-27 19:19:24 +0530

    Add icons and some basic functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-// function App() {
-//   return (
-//     <div className="My_planets">
-//       "Earth", "Mars", "Jupiter", "Venus", "Saturn", "SMAC", "Neptune", "Pluto",
-//       "Sun"
-//     </div>
-//   );
-// }
-
-// export default App;
-
+import earth from "./assets/images/icon-earth.png";
+import globe from "./assets/images/icon-globe.png";
+import jupiter from "./assets/images/icon-jupiter.png";
+import mars from "./assets/images/icon-mars.png";
+import mercury from "./assets/images/icon-mercury.png";
+import neptune from "./assets/images/icon-neptune.png";
+import saturn from "./assets/images/icon-saturn.png";
+import uranus from "./assets/images/icon-uranus.png";
+import venus from "./assets/images/icon-venus.png";
 function App() {
   let planets = [
-    "Earth",
-    "Mars",
-    "Jupiter",
-    "Venus",
-    "Saturn",
-    "SMAC",
-    "Neptune",
-    "Pluto",
-    "Sun",
+    {
+      title: "Earth",
+      image: { earth },
+    },
+    {
+      title: "Mars",
+      image: { mars },
+    },
+    {
+      title: "Jupiter",
+      image: { jupiter },
+    },
+    {
+      title: "Venus",
+      image: { venus },
+    },
+    {
+      title: "Saturn",
+      image: { saturn },
+    },
+    {
+      title: "SMAC",
+      image: { mercury },
+    },
+    {
+      title: "Neptune",
+      image: { neptune },
+    },
+    {
+      title: "Uranus",
+      image: { uranus },
+    },
+    {
+      title: "Sun",
+      image: { globe },
+    },
   ];
   return (
     <div className="body">
