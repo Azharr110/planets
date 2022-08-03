065434dc2d175f6e6b5b6be4482721e01bff1d78
Azhar <abdazhar786@gmail.com>
2022-08-03 13:27:58 +0530

    Add VenusModal

---
+++ src/VenusModal.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Button, Modal, ModalFooter, ModalHeader, ModalBody } from "reactstrap";
+import venus from "./assets/images/icon-venus.png";
+
+export function VenusModal() {
+  // Modal open state
+  const [modal, setModal] = useState(false);
+
+  // Toggle for Modal
+  const toggle = () => setModal(!modal);
+  return (
+    <div
+      style={{
+        display: "block",
+        color: "white",
+        width: 100,
+        padding: 30,
+      }}
+    >
+      <Button color="danger" onClick={toggle}>
+        Click me to know more about Venus
+      </Button>
+      <Modal
+        style={{
+          backgroundColor: "white",
+          display: "block",
+          width: 700,
+          padding: 30,
+        }}
+        isOpen={modal}
+        toggle={toggle}
+      >
+        <ModalHeader style={{ color: "black" }}>
+          <h2>Venus</h2>
+        </ModalHeader>
+        <img src={venus} height="150px" width="150px" />
+
+        <ModalBody style={{ color: "black", float: "right" }}>
+          It has a solid surface that is covered with craters like our Moon. It
+          has a thin atmosphere, and it doesn't have any moons. Mercury likes to
+          keep things simple. Mercury spins slowly compared to Earth, so one day
+          lasts a long time. Mercury takes 59 Earth days to make one full
+          rotation.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            Close
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+export default VenusModal;
